0ca07bac56ca638bcf967635b5cdf281fc746996
mingbai <baimingmin@hotmail.com>
2018-02-14 23:22:00 -0600

    UnionFind

--- DataStructures/UnionFind.java
+++ DataStructures/UnionFind.java
@@ -1,18 +1,32 @@
 /* UnionFind
 Union-Find is a data structure that maintains disjoint set(connected components) memberships.
-And make it easier to merge (Union) two components and find if two components are connected ot not.
-
-
+And make it easier to merge (Union) two components and find if two components are connected or not.
 
 */
 
 class UnionFind {
 
-	public void Union() {
+	int i;
+	int[] father;
 
+	UnionFind(int i){
+		father = new int[i];
 	}
 
-	public int Find() {
+	public void Union(int a, intb) {
+		int f_a = father[a];
+		int f_b = father[b];
+		if(f_a != f_b) {
+			father[f_a] = f_b; 
+		}
+	}
 
+	public int Find(int a) {
+		if (x == father[x]) {
+			return x;
+		} else {
+			return find(father[x]);
+		}
 	}
 }
+
